5f4f732269270669c1a02a8024ab4b773eb9a83d
Sam <sh3244@nyu.edu>
2020-01-09 13:01:01 -0500

    Initial Commit

---
+++ SKit/SKit.h
@@ -0,0 +1,19 @@
+//
+//  SKit.h
+//  SKit
+//
+//  Created by Sam on 1/9/20.
+//  Copyright © 2020 shny. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SKit.
+FOUNDATION_EXPORT double SKitVersionNumber;
+
+//! Project version string for SKit.
+FOUNDATION_EXPORT const unsigned char SKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SKit/PublicHeader.h>
+
+
